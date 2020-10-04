8597422291c1af217227cb4fba9b7479c59a8808
Akhil <argangul@lowes.com>
2020-10-04 13:47:15 +0530

    added log

--- src/services/diet/planner.service.js
+++ src/services/diet/planner.service.js
@@ -4,7 +4,7 @@ const httpStatus = require("http-status");
 const MODModel = require("../../models/diet/mod.model");
 const FoodModel = require("../../models/diet/food.model");
 const ApiError = require("../../utils/ApiError");
-
+const logger = require("./../../config/logger")
 const createDayPlan = async (payload) => {
   const plan = new MODModel[payload.collection](payload);
   await plan.save();
@@ -13,6 +13,26 @@ const createDayPlan = async (payload) => {
 
 const getPlan = async ({ collection, week }) => {
   const MODPlan = await MODModel[collection].findOne({ week }).lean();
+  if (!MODPlan) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Plan not found");
+  }
+  logger.debug("Normalizing the response: " + JSON.stringify(MODPlan));
+  for (let planIndex = 0; planIndex < MODPlan.plans.length; planIndex++) {
+    const plan = MODPlan.plans[planIndex];
+    for (let mealIndex = 0; mealIndex < plan.meals.length; mealIndex++) {
+      const items = plan.meals[mealIndex].items;
+      for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+        let menu = await FoodModel.findOne({ _id: items[itemIndex].id }).lean();
+        items[itemIndex] = { ...items[itemIndex], ...menu };
+      }
+    }
+  }
+  logger.debug("Normalized response: " + JSON.stringify(MODPlan));
+  return MODPlan;
+};
+
+const getPlanById = async ({ id, collection }) => {
+  const MODPlan = await MODModel[collection].findById(id).lean();
   if (!MODPlan) {
     throw new ApiError(httpStatus.NOT_FOUND, "Plan not found");
   }
@@ -27,23 +47,6 @@ const getPlan = async ({ collection, week }) => {
     }
   }
   return MODPlan;
-};
-
-const getPlanById = async ({ id, collection }) => {
-  const plan = await MODModel[collection].findById(id).exec();
-  let item = plan.plans[0].meals[0].items;
-  for (let i = 0; i < item.length; i = i + 1) {
-    let menu = await FoodModel.find(
-      { _id: item[i].id },
-      { substitute: 0 }
-    ).exec();
-    let menuItem = { ...menu };
-    item[i] = { item: item[i], menu: menuItem[0] };
-  }
-  if (!plan) {
-    throw new ApiError(httpStatus.NOT_FOUND, "Plan not found");
-  }
-  return plan;
 };
 
 const deleteDayPlanById = async ({ id, collection }) => {
